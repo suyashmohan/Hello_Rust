bd9d4fbb2615229476c066c3d68eb13e5a048e67
Suyash Mohan <suyashatweb@gmail.com>
2021-03-18 16:50:48 +0800

    trait and generics

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,34 @@
-use std::collections::HashMap;
+use std::error::Error;
+use std::ops::Mul;
 
-fn main() {
-    let mut v: Vec<u32> = vec![1,2,3];
-    v.push(4);
-    v.push(5);
-    for i in &mut  v {
-        *i *= 10;
-        println!("{}", i)
-    }
+trait Area<T> {
+    fn area(self) -> T
+    where
+        T: Mul<Output = T>;
+}
 
-    #[derive(Debug)]
-    enum SpreadsheetCell {
-        Int(i32),
-        Float(f64),
-        Text(String),
-    }
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12),
-    ];
-    for i in row {
-        println!("{:?}", i);
-    }
+struct Point<T> {
+    x: T,
+    y: T,
+}
 
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("world!");
-    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
-    // let s3 = String::new() + &s1 + &s2; // this should create an empty string and then copy s1 and s2 to it and return it
-    println!("{}", s3);
-    let hello = "Здравствуйте";
-    let s = &hello[0..4];
-    println!("{} - {}", s, hello);
-    for c in "नमस्ते".chars() {
-        println!("{}", c);
+impl<T> Point<T> {
+    fn new(x: T, y: T) -> Point<T> {
+        Point { x, y }
     }
+}
 
-    let mut scores = HashMap::new();
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
-    scores.insert(String::from("Blue"), 25); // Udpate Blue
-    scores.entry(String::from("Yellow")).or_insert(50); // Only insert if doesn't exist
-    let team_name = String::from("Blue");
-    let score = scores.get(&team_name).unwrap();
-    println!("{} - {}", team_name, score);
-    for (key, value) in &scores {
-        println!("{}: {}", key, value);
+impl<T> Area<T> for Point<T> {
+    fn area(self) -> T
+    where
+        T: Mul<Output = T>,
+    {
+        return self.x * self.y;
     }
+}
 
-    let text = "hello world wonderful world";
-    let mut map = HashMap::new();
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
-        *count += 1;
-    }
-    println!("{:?}", map);
+fn main() -> Result<(), Box<dyn Error>> {
+    let point = Point::new(10.0, 20.0);
+    println!("Area is {}", point.area());
+    Ok(())
 }
